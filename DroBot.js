104f20527f40cc03cc4c49ba48db3be5480ff253
breezecakeyum <breezeownzu@gmail.com>
2016-11-19 14:07:50 -0800

    Rev 5

--- DroBot.js
+++ DroBot.js
@@ -1,6 +1,7 @@
 var Moment = require('moment-timezone');
 var Discord = require('discord.io');
 
+
 var bot = new Discord.Client({
     token: "MjQzMjMxOTAzMzU4OTEwNDY0.CvsCKA.jXhaUfYzzok5FT3tUsPU1Uhzf1c",
     autorun: true
@@ -13,105 +14,62 @@ bot.on("any", function(event) {
 });
 
 bot.editUserInfo({
-    avatar: require('fs').readFileSync('C:/Users/breeze/node_modules/discord.io/images/robo.jpg', 'base64'), //Optional
+    "avatar": "require('fs').readFileSync('C:/Users/breeze/node_modules/discord.io/images/robo.jpg', 'base64')" //Optional
 })
 
 bot.on('ready', function() {
     console.log(bot.username + " - (" + bot.id + ")");
 });	
 
+//Command Bounce
+bot.on('message', function(user, userID, channelID, message, event) {
+    if (message === "!commands") {
+        bot.sendMessage({
+            to: channelID,
+            message: "```!logs | !gear | !attendance | !pawn | !simcraft | !resources | !website```"
+        });
+    }
+});
+
 //Text Bounce
 bot.on('message', function(user, userID, channelID, message, event) {
     if (message === "!logs") {
         bot.sendMessage({
             to: channelID,
-            message: "Reign of Fire Logs >>> https://www.warcraftlogs.com/guilds/5151\nArea 52 Emerald Nightmare Logs >>> https://www.warcraftlogs.com/rankings/server/18/10\nEmerald Nightmare 95% Logs >>> https://www.warcraftlogs.com/statistics/10#dataset=95"
+            message: "***Reign of Fire Logs >>> https://www.warcraftlogs.com/guilds/5151 \nArea 52 Emerald Nightmare Logs >>> https://www.warcraftlogs.com/rankings/server/18/10 \nEmerald Nightmare 95% Logs >>> https://www.warcraftlogs.com/statistics/10#dataset=95***"
         });
     }
-	if (message === "lol"){
+	if (message === "!gear"){
 		bot.sendMessage( {
 			to: channelID,
-			message: "Haha!"
+			message: "***Reign of Fire Loot Spreadsheet >>> https://docs.google.com/spreadsheets/d/1vhZwxWMENb6wseDP4J5NHvls-_hmYnoIiTAc-52CxOY \nReign of Fire Loot History >>> https://docs.google.com/spreadsheets/d/1YDDrJsaQK_j2kyV6EfSc9e6XugI9IXUMLtXuzng5YwQ***"
 		});
 	}
+	if (message === "!attendance"){
+		bot.sendMessage( {
+			to: channelID,
+			message: "***Reign of Fire Attendance >>> https://docs.google.com/spreadsheets/d/13C-8hPqPEA3n46nRiunYn4gGeCgbL05Jjx5YoMV_vWU***"
+		});
+	}
+	if (message === "!pawn" || message === "!simcraft"){
+		bot.sendMessage( {
+			to: channelID,
+			message: "***https://www.youtube.com/watch?v=kPvjkW0ZkxM***"
+		});
+	}
+	if (message === "!resources"){
+		bot.sendMessage( {
+			to: channelID,
+			message: "***Legion To-Do List >>> https://docs.google.com/spreadsheets/d/1TiiI4huz4NXKfx7PGvvVTyP6xu8VaDNZ4-FEn69mOhc \nArtifact Power >>> https://docs.google.com/spreadsheets/d/11xQCzhiVM9gTZkUsu1UNs7z4dpwHpRQSUUZySY7xLv0***"
+		});
+	}	
+	if (message === "!website"){
+		bot.sendMessage( {
+			to: channelID,
+			message: "***Guild Website >>> rof.guildlaunch.com***"
+		});
+	}	
 });
-
-// bot.on('any', function(channelID, rawEvent) {
-    // if (channelID === 244132831662440451) {
-		// var arr = ['beep1.jpg', 'beep2.jpg', 'beep3.jpg'];
-		// var i = 0; 
-		
-		// while (i < arr.length){
-			// var intervalID = setInterval (function (){
-				// bot.sendMessage({to: channelID, message: arr[i]});
-				// sendFiles(channelID, [arr[i]]);
-				// i++;
-			// }, 10000);
-		// }
-		
-		// i = 0;
-    // }
-// });
-
-// bot.on('message', function(user, userID, channelID, message, rawEvent) {
-	// var currentTime = new Date().toLocaleTimeString('en-US', {hour12:true});
-	// var estTime = Moment().tz(currentTime, "America/New_York");
-	
-    // if (message === "!start") {
-      // var intervalID = setInterval (function (){
-		// sendFiles(channelID, ["clock.png"]);
-		
-		// bot.sendMessage({
-			// to: channelID,
-			// message: "Current EST time is: " + estTime
-		// });	
-		// bot.sendMessage({
-			// to: channelID,
-			// message: "Current Dronai time is: " + currentTime
-		// });
-      // }, 10000);
-    // }
-	// else if (message === "!stop"){
-		// clearInterval(intervalID);
-	// }
-// });
-
-//Display Current Time and Locale time
-// bot.on('message', function(user, userID, channelID, message, rawEvent) {
-	// var currentTime = new Date().toLocaleTimeString('en-US', {hour12:true});
-	// var estTime = Moment().tz(currentTime, "America/New_York");
-	
-    // if (message === "!time") {
-		// sendFiles(channelID, ["clock.png"]);
-		
-		// bot.sendMessage({
-			// to: channelID,
-			// message: "Current EST time is: " + estTime
-		// });	
-		// bot.sendMessage({
-			// to: channelID,
-			// message: "Current Dronai time is: " + currentTime
-		// });
-    // }
-// });
-
-// bot.on("message", function(user, userID, channelID, message, event) {
-    // if (message === "!Live") {
-        // getJSON("https://api.twitch.tv/kraken/streams/breezecakeyum", function(err, res) {
-            // if (res.stream == null) {
-                // bot.sendMessage({
-					// to: channelID,
-					// message: "is not live!"
-				// });	
-            // } else {
-                // bot.sendMessage({
-					// to: channelID,
-					// message: "is live!"
-				// });	
-            // }
-        // });
-    // }
-// });
 
 // Obviously a function to send files
 function sendFiles(channelID, fileArr, interval) {
